944afaf7f963f465c47e69e17b0a998686ccc072
vadim <sudkurve@mail.ru>
2021-07-13 17:23:32 +0300

    cd(cd): deploy test #4

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import express from "express";
-import dotenv from "dotenv";
-import mongoose from "mongoose";
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const express = require("express");
 
 dotenv.config();
 
