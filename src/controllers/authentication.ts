593307707b57f481e1c091f2aa34f7d2ed96f971
vadim <sudkurve@mail.ru>
2021-07-26 00:39:48 +0300

    feat(auth): added hashing password & existing email check #6

--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -1,26 +1,35 @@
 import { ITokens } from "../types";
+
+const bcrypt = require("bcrypt");
 const authService = require("../services/auth-service");
-const User = require("../models/user");
+const UserModel = require("../models/user");
+const TokensModel = require("../models/tokens");
+
+const MAX_AGE = 30 * 24 * 60 * 60 * 1000;
 
 class Authentication {
   async register(req, res, _next) {
     try {
       const { email, password, nickname } = req.body;
 
-      if (!email)
-        return res
-          .status(400)
-          .json({ message: "Email field is empty or uncorrect" });
+      const hashPassword = await bcrypt.hash(password, 4);
+      const candidate = await UserModel.findOne({ email });
 
-      const user = new User({ email, password, nickname });
+      if (candidate) throw new Error("User with such email already exists");
+
+      const user = new UserModel({ email, password: hashPassword, nickname });
       await user.save();
 
       const tokens: ITokens = authService.createTokens(req.body);
 
+      const createdTokens = new TokensModel({ user: user._id, ...tokens });
+      await createdTokens.save();
+
       res.cookie("refreshToken", tokens.refreshToken, {
-        maxAge: 30 * 24 * 60 * 60 * 1000,
+        maxAge: MAX_AGE,
         httpOnly: true,
       });
+
       res.json({ email, nickname, id: user._id, tokens });
     } catch (e) {
       const { message } = e;
@@ -31,19 +40,47 @@ class Authentication {
   }
 
   login(req, res, _next) {
-    res.json({ method: "login" });
+    try {
+      res.json({ method: "login" });
+    } catch (e) {
+      const { message } = e;
+      console.error("ERROR: ", message);
+
+      res.status(400).json({ message });
+    }
   }
 
   logout(req, res, _next) {
-    res.json({ method: "logout" });
+    try {
+      res.json({ method: "logout" });
+    } catch (e) {
+      const { message } = e;
+      console.error("ERROR: ", message);
+
+      res.status(400).json({ message });
+    }
   }
 
   activate(req, res, _next) {
-    res.json({ method: "activate" });
+    try {
+      res.json({ method: "activate" });
+    } catch (e) {
+      const { message } = e;
+      console.error("ERROR: ", message);
+
+      res.status(400).json({ message });
+    }
   }
 
   refresh(req, res, _next) {
-    res.json({ method: "refresh" });
+    try {
+      res.json({ method: "refresh" });
+    } catch (e) {
+      const { message } = e;
+      console.error("ERROR: ", message);
+
+      res.status(400).json({ message });
+    }
   }
 }
 
