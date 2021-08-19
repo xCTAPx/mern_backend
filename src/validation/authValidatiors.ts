7189e5206ebe55139044c281f7bcb05a8c763c30
vadim <sudkurve@mail.ru>
2021-08-20 00:28:21 +0300

    refactor(validation): changed auth validation values

--- src/validation/authValidatiors.ts
+++ src/validation/authValidatiors.ts
@@ -8,11 +8,11 @@ export const VALIDATORS = {
     .normalizeEmail()
     .withMessage("Incorrect email format"),
   nicknameValidator: body("nickname")
-    .isLength({ min: 5 })
-    .withMessage("Nickname must be at least 5 chars long"),
+    .isLength({ min: 5, max: 20 })
+    .withMessage("Nickname must be at least 5 and max 20 chars long"),
   passwordValidator: body("password")
-    .isLength({ min: 8 })
-    .withMessage("Password must be at least 8 chars long")
+    .isLength({ min: 8, max: 32 })
+    .withMessage("Password must be at least 8 and max 32 chars long")
     .custom((value: string) => value.match(passwordRegExp))
     .withMessage(
       "Password must include spec symbols, latin characters or numbers only"
